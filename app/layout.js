90d3cca06843c7e689ca679224fcb34e2d1d0a7c
nmrcharlee-oss <nmrcharlee@gmail.com>
2026-01-23 16:08:18 +0600

    layout.js

---
+++ app/layout.js
@@ -0,0 +1,26 @@
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["300", "400", "600", "700"],
+  variable: "--font-poppins",
+});
+
+export const metadata = {
+  title: "Bhoot.com",
+  description: "Listen to the latest horror stories.",
+  icons: {
+    icon: 'https://beeimg.com/images/o68822330973.png', 
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.variable} font-sans antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
