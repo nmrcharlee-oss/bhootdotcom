938adf3e747292a42f504b16f0ac329022a6449a
nmrcharlee-oss <nmrcharlee@gmail.com>
2026-01-23 22:16:05 +0600

    next.config.js

--- next.config.js
+++ next.config.js
@@ -2,11 +2,24 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'beeimg.com',
-      },
+      { protocol: 'https', hostname: 'beeimg.com' },
     ],
+  },
+  // Security Headers
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'X-DNS-Prefetch-Control', value: 'on' },
+          { key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload' },
+          { key: 'X-XSS-Protection', value: '1; mode=block' },
+          { key: 'X-Frame-Options', value: 'SAMEORIGIN' }, // Prevents clickjacking
+          { key: 'X-Content-Type-Options', value: 'nosniff' }, // Prevents MIME sniffing
+          { key: 'Referrer-Policy', value: 'origin-when-cross-origin' },
+        ],
+      },
+    ];
   },
 };
 
