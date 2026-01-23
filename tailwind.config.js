d6005c9148971b2b262c132de03684b6eb46d158
nmrcharlee-oss <nmrcharlee@gmail.com>
2026-01-23 17:40:16 +0600

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', // <--- This is new and important
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,6 +9,23 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['var(--font-poppins)'],
+      },
+      animation: {
+        'blob': 'blob 10s infinite',
+        'shine': 'shine 1.5s infinite',
+      },
+      keyframes: {
+        blob: {
+          '0%': { transform: 'translate(0px, 0px) scale(1)' },
+          '33%': { transform: 'translate(30px, -50px) scale(1.1)' },
+          '66%': { transform: 'translate(-20px, 20px) scale(0.9)' },
+          '100%': { transform: 'translate(0px, 0px) scale(1)' },
+        },
+        shine: {
+          '0%': { transform: 'skewX(-15deg) translateX(-150%)' },
+          '50%': { transform: 'skewX(-15deg) translateX(150%)' },
+          '100%': { transform: 'skewX(-15deg) translateX(150%)' },
+        }
       },
     },
   },
